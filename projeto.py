8ebce11bd8065344c60f5810b8ab46bae7ee0e41
Matheus Wander <96889543+OMatheusWander@users.noreply.github.com>
2026-06-13 09:12:16 -0300

    Enhance projeto.py with comments
    Added comments for better code understanding

--- projeto.py
+++ projeto.py
@@ -1,13 +1,17 @@
-import telebot
+import telebot # importação de telebot
 
-bot = telebot.TeleBot('8996493505:AAHKdGUbVC_m004C04n5EkqXEy9AApAfNBc')
+bot = telebot.TeleBot('8996493505:AAHKdGUbVC_m004C04n5EkqXEy9AApAfNBc') #chave API
 
-@bot.message_handler(['start', 'help'])
+@bot.message_handler(['start', 'help']) #manipulação básica do chatbot
+
+#função categoria
 def start(msg):
     bot.reply_to(msg, 'Qual categoria você gastou?')
 
 bot.infinity_polling()
 
+
+#função receber categoria
 def receber_categoria(msg):
     categoria = msg.text
     bot.reply_to(msg, f'Categoria escolhida: {categoria}')
